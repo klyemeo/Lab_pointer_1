87a0ff9bfa7e3d0a105971d1cbe0491433873933
kyle <thonjarkrasri@gmail.com>
2026-02-02 21:16:14 +0700

    gggg

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,19 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a,int &b, int &c, int &d){
+	int gong[4] = {a,b,c,d};
+
+	for (int i = 0 ; i < 4 ; i++){
+		int randomss = rand()%4 ;
+		int temp = gong[i];
+		gong[i] = gong[randomss];
+		gong[randomss] = temp ; 
+
+	}
+	a = gong[0]; 
+	b = gong[1];
+	c = gong[2];
+	d = gong[3];
+}
--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,19 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+
+void shuffle(int *a,int *b, int *c, int *d){
+	int gong[4] = {*a,*b,*c,*d};
+
+	for (int i = 0 ; i < 4 ; i++){
+		int randomss = i + rand() % (4 - i);
+		int temp = gong[i];
+		gong[i] = gong[randomss];
+		gong[randomss] = temp ; 
+
+	}
+	*a = gong[0]; 
+	*b = gong[1];
+	*c = gong[2];
+	*d = gong[3];
+}
